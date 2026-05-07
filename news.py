0ad4b266d9c64755f08d9a949b13f530e2d72d70
Isaiah Williams-Bell <isaiah@terpmail.umd.edu>
2026-05-06 23:27:45 -0400

    Created gui and export button with messages success/fail for scraping headline based on link

--- news.py
+++ news.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import tkinter as tk
+import csv
+import os
 
 # Allowed sites
 # https://www.bbc.com/business
@@ -8,18 +10,93 @@ import tkinter as tk
 # https://www.npr.org/sections/business/
 
 # Requesting website
-URL_web = input("Enter URL: ")
-headers = {"User-Agent": "Mozilla/5.0"}
-response = requests.get(URL_web, headers=headers)
-print("The response code is : ", response)
+# URL_web = input("Enter URL: ")
+
+
+#creating window for scraper
+root = tk.Tk()
+root.title("News Sentiment")
+root.geometry("800x800")
+
+#input box
+titleURL_label = tk.Label(root, text="Enter News URL (USE BBC, CNN, NPR links)")
+titleURL_label.pack()
+url_entry = tk.Entry(root, width=50,)
+url_entry.pack(pady=10)
+
+#output box
+output = tk.Text(root, height=10, width=90)
+output.pack(pady=10)
+
+headlines_list = [] #headline list to store from websites
+
+def scrape():
+    global headlines_list
+    headlines_list = []
+    url = url_entry.get().strip()
+
+    headers = {"User-Agent": "Mozilla/5.0"}
+
+    try:
+        response = requests.get(url, headers=headers)
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+        headlines = soup.find_all('h1')
+
+        output.delete("1.0", tk.END)
+        
+
+        for h in headlines:
+            text = h.text.strip()
+            headlines_list.append(text)
+            output.insert(tk.END, h.text.strip() + "\n\n")
+
+    except Exception as e:
+        output.delete("1.0", tk.END)
+        output.insert(tk.END, f"Error: {e}")
+        
+def export_csv():
+    try:
+        file_path = os.path.abspath("headlines.csv")
+
+        with open(file_path, "w", newline="", encoding="utf-8") as file:
+            writer = csv.writer(file)
+            writer.writerow(["Headline"])
+
+            for item in headlines_list:
+                writer.writerow([item])
+
+        status_label.config(
+            text=f"Export successful! Saved to:\n{file_path}",
+            fg="green"
+        )
+
+    except Exception as e:
+        status_label.config(
+            text=f"Export failed: {e}",
+            fg="red"
+        )
+
+#headers = {"User-Agent": "Mozilla/5.0"}
+#response = requests.get(URL_web, headers=headers)
+#print("The response code is : ", response)
 
 #Parsing HTML Document
-soup=BeautifulSoup(response.content,'html.parser')
+#soup=BeautifulSoup(response.content,'html.parser')
 
 #Extract headline
-headlines=soup.find_all('h1')
-for headline in headlines:
-    print(headline.text, "\n")
+#headlines=soup.find_all('h1')
+#for headline in headlines:
+#    print(headline.text, "\n")
     
-#Test comment
+export_button = tk.Button(root, text="Export CSV", command=export_csv)
+export_button.pack()
+
+button = tk.Button(root, text="Scrape", command=scrape)
+button.pack()
+
+status_label = tk.Label(root, text="")
+status_label.pack()
+
+root.mainloop()
     
